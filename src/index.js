a0cf517012b029883bfee2c178f766caf13e780c
Alex Aguiar <alex3aguiar@gmail.com>
2023-09-30 02:11:10 -0300

    asdÄS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 3000;
 const path = require('path')
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
@@ -17,6 +22,6 @@ io.on('connection', (socket) => {
 
 
 
-http.listen(port, host, () => {
+server.listen(port, host, () => {
   console.log(`Socket.IO server running at http://${host}:${port}/`);
 });
