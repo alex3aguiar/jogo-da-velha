d788d51d3ceb3810168edf0bf155036f77e2a8a2
Alex Aguiar <alex3aguiar@gmail.com>
2023-09-30 00:58:21 -0300

    asdÄS

--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// io.on('connection', (socket) => {
-//   socket.on('chat message', msg => {
-//     io.emit('chat message', msg);
-//   });
-// });
-
-http.listen(port, host, () => {
-  console.log(`Socket.IO server running at http://${host}:${port}/`);
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
 });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
